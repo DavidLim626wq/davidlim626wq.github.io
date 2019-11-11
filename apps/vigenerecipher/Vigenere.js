7e7f1ceca17e2a2b7d50d28256c9a59c6629e2c5
david <lin.dewei1@gmail.com>
2019-11-11 22:41:42 +0800

    Better Layout

--- apps/vigenerecipher/Vigenere.js
+++ apps/vigenerecipher/Vigenere.js
@@ -1,14 +1,28 @@
+function isAlpha(str) {
+  return /^[a-zA-Z]+$/.test(str);
+}
+
 function vigenere(str, key){
 	//strip whitespace and punctuation
 	var ciphertext = '';
-	str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"").toUpperCase();
+	str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\1-9s]/g,"").toUpperCase();
+	key = key.toUpperCase();
+	if (isAlpha(key) == false)
+	{
+		ciphertext = "Error: Key can only contain letters A-Z";
+	}
+	else
+	{
 	for(var i = 0; i < str.length; i++){
 	shift = key[i % key.length].charCodeAt(0) - 65;
 	c = ((str[i].charCodeAt(0) - 65) + shift) % 26;
 	ciphertext += String.fromCharCode(c + 65);
 	}
+	}
 	return(ciphertext)
 }
+
+
 
 $(function(){
 	$('button').click(function(){
@@ -16,5 +30,6 @@ $(function(){
 		var m = $("#plainText").val();
 		ct = vigenere(m, k);
 		$("#ciphertext").text(ct);
+		$("#key").text(k);
 		});
 	});
